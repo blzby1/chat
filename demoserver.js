c93f4d10be977645170ad0c5f50d6c5e2b29b9a7
blzby1 <96076743+blzby1@users.noreply.github.com>
2023-12-20 16:30:51 +0000

    make a barely functioning chat

--- demoserver.js
+++ demoserver.js
@@ -2,9 +2,13 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import fs from 'node:fs';
-import { match } from "assert";
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
 const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
@@ -13,6 +17,7 @@ const count = io.engine.clientsCount;
 
 app.use('/', express.static("./src/css"));
 app.use('/', express.static("./src/png"));
+app.use('/', express.static('./src/js'))
 
 app.route('/').get((req, res) => {
   const container = fs.readFileSync('./src/html/fullscreencontainer.html');
@@ -20,11 +25,18 @@ app.route('/').get((req, res) => {
 
   const edited = container.toString().replace(/<!-- placeholder -->/, chat);
 
+  res.setHeader("Content-Type", "text/html");
   res.send(edited);
 });
 
-io.on("connection", (socket) => {
-  // ...
+app.route('/socket.io.js').get((req, res) => {
+  res.sendFile(join(__dirname, '/node_modules/socket.io/client-dist/socket.io.js'));
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 httpServer.listen(8080);
